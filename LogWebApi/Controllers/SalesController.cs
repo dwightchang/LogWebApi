1d9505709442ded39b5322dc7ef5948792499c3c
Dwight Chang <dweightchang@hotmail.com>
2022-01-29 21:50:45 +0800

    ProxyFactory

--- LogWebApi/Controllers/SalesController.cs
+++ LogWebApi/Controllers/SalesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiProxy.Proxy;
+using LogWebApi.Model;
 using LogWebApi.Service;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -13,12 +14,7 @@ namespace LogWebApi.Controllers
     {
         public IActionResult SalesDashboard()
         {
-            OrderProxy proxy = new OrderProxy("http://localhost:63392", "f496896f-446f-4a5b-97c4-b6d12f66a22c")
-            {
-                Logger = SysLogger.ApiElapsedTime
-            };
-
-            var orderData = proxy.FindOrder(new ApiProxy.Models.Order.FindOrderReq()
+            var orderData = ProxyFactory.OrderProxy().FindOrder(new ApiProxy.Models.Order.FindOrderReq()
             {
                 OrderSn = 1
             });
---
+++ LogWebApi/Model/ProxyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiProxy.Proxy;
+
+namespace LogWebApi.Model
+{
+    public class ProxyFactory
+    {
+        public static OrderProxy OrderProxy()
+        {
+            return new OrderProxy("http://localhost:63392", "f496896f-446f-4a5b-97c4-b6d12f66a22c");
+        }
+    }
+}
